1c971f39dfb2c92db00b4e3bd42b76780e72c8be
Ladis <Ladislav-5@seznam.cz>
2024-09-24 10:47:10 +0200

    Style

--- viewer/views.py
+++ viewer/views.py
@@ -1,14 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template.context_processors import request
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import UserRegisterForm
+
 
 def main(request):
     return render(request, 'viewer/main.html')
 
 def login(request):
-    return render(request, 'login.html')
+    return render(request, 'viewer/login.html')
 
 def register(request):
-    return render(request, 'register.html')
+    return render(request, 'viewer/register.html')
 
 def auctions(request):
-    return render(request, 'auctions.html')
+    return render(request, 'viewer/auctions.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Váš účet byl vytvořen, {username}! Nyní se můžete přihlásit.')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'viewer/register.html', {'form': form})
