aeeb45d07282b231ada32c146cb92f61cf79534e
Ladis <Ladislav-5@seznam.cz>
2024-10-21 16:56:02 +0200

    Final changes

--- viewer/templatetags/group_tags.py
+++ viewer/templatetags/group_tags.py
@@ -1,7 +1,10 @@
 from django import template
+from django.contrib.auth.models import Group
 
 register = template.Library()
 
 @register.filter(name='has_group')
 def has_group(user, group_name):
-    return user.groups.filter(name=group_name).exists()
+    group = Group.objects.filter(name=group_name).first()
+    return group in user.groups.all() if user.is_authenticated and group else False
+
